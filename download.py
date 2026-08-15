a6d8c987488f0874e0f4f90e97ecc85167864668
NARENDRA G S <narenskills@gmail.com>
2026-08-15 09:22:08 +0530

    downlading dataset

---
+++ download.py
@@ -0,0 +1,3 @@
+medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
+from urllib.request import urlretrieve
+urlretrieve(medical_charges_url, 'medical.csv')
